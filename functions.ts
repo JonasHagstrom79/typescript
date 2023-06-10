1cf814a061f2e492e0d416232c1b061e8285daec
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-06-10 23:48:28 +0200

    annotations

--- functions.ts
+++ functions.ts
@@ -10,3 +10,11 @@ function square(num: number){
 function greet(person: string = "Default value") {
     return "Hello " + person;
 }
+
+// Creating a function with a return type
+const addNums = (x: number, y: number): number => {
+    return x + y;
+}
+
+const add = (x: number, y: number): number => {
+    return x + y; }
